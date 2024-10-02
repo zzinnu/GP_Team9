9d09e8dc0df3a0ab52eeeceee5719d4646d55b89
abh <themechanic002@gmail.com>
2024-10-02 20:18:52 +0900

    Add: Ground check and Horizontal movement setting

--- Assets/+---Level/I +---Prefabs/Player/PlayerController.cs
+++ Assets/+---Level/I +---Prefabs/Player/PlayerController.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public Rigidbody2D rb;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +25,51 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        if(horizontal > 0f && !isFacingRight)
+        {
+            Flip();
+        }
+        else if(horizontal < 0f && isFacingRight)
+        {
+            Flip();
+        }
     }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    // Flip
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+
+    // Jump
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(context.performed && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (context.canceled && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+
+    // Player Movement
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+    
+
 }
